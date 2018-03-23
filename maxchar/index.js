70b77abe7b296ec84ae03346be58feecb0be6da6
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-03-23 09:34:12 -0500

    find character with max occurence problem

---
+++ maxchar/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+    const charMap = {}
+    let max = 0
+    let maxChar = ''
+    for (let char of str){
+        if(charMap[char]) {
+            charMap[char] ++
+        }
+        else{
+            charMap[char] = 1
+        }
+    }
+
+    for (let char in charMap){
+        if(charMap[char] > max){
+            max = charMap[char]
+            maxChar = char
+        }
+        else{
+            max ++
+        }
+    }
+
+    return maxChar
+    console.log(max, maxChar)
+
+}
+module.exports = maxChar;

5a26631dae7964962f6ceb6ea4d7bcc950395a03
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-03-26 10:12:12 -0500

    alternative solutions to anagram problem

--- anagrams/index.js
+++ anagrams/index.js
@@ -7,40 +7,66 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
 
-    // using regular expressions to remove spaces, !, etc. and making string lowercase
-    let convertedA = stringA.replace(/[^\w]/g, '').toLowerCase()
-    let convertedB = stringB.replace(/[^\w]/g, '').toLowerCase()
+// ******************************************* Solution 1 ********************************************
+// function anagrams(stringA, stringB) {
 
-    const charMapA = map(convertedA)
-    const charMapB = map(convertedB)
+//     // using regular expressions to remove spaces, !, etc. and making string lowercase
+//     let convertedA = stringA.replace(/[^\w]/g, '').toLowerCase()
+//     let convertedB = stringB.replace(/[^\w]/g, '').toLowerCase()
 
-    console.log(charMapA) 
-    console.log(charMapB) 
+//     const charMapA = map(convertedA)
+//     const charMapB = map(convertedB)
 
-    return compareMaps(charMapA, charMapB)
-}
+//     console.log(charMapA) 
+//     console.log(charMapB) 
 
-function map(str){
-    const charMap = {}
+//     return compareMaps(charMapA, charMapB)
+// }
 
-    for (let char of str) {
-        charMap[char] = charMap[char] + 1 || 1
-    }
+// function map(str){
+//     const charMap = {}
 
-    return charMap
-}
+//     for (let char of str) {
+//         charMap[char] = charMap[char] + 1 || 1
+//     }
 
-function compareMaps(charMapA, charMapB) {
+//     return charMap
+// }
+
+// function compareMaps(charMapA, charMapB) {
     
-    if(Object.keys(charMapA).length !== Object.keys(charMapB).length) return false
-    else{
-        for(let char in charMapA){
-            if(charMapA[char] !== charMapB[char]) return false
-        }
-        return true
-    }
+//     if(Object.keys(charMapA).length !== Object.keys(charMapB).length) return false
+//     else{
+//         for(let char in charMapA){
+//             if(charMapA[char] !== charMapB[char]) return false
+//         }
+//         return true
+//     }
+// }
+
+// ******************************************* Solution 2 ********************************************
+
+// function anagrams(stringA, stringB) {
+//     let convertedA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort()
+//     let convertedB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort()
+
+//     if(convertedA.length !== convertedB.length) return false
+
+//     for(let i in convertedA){
+//         if(convertedA[i] !== convertedB[i]){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// ******************************************* Solution 3 ********************************************
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
 }
 
+function cleanString(str){
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 module.exports = anagrams; 

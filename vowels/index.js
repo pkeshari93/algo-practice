683f712a094e31f54a1b569836e26c115ac658f1
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-03 09:18:59 -0500

    vowels iterative solution 2

--- vowels/index.js
+++ vowels/index.js
@@ -7,14 +7,28 @@
 //   vowels('Why?') --> 0
 
 // ************************************** Solution 1 ********************************************
+// function vowels(str) {
+//     let counter = 0
+//     for (let char in str){
+//         if (str[char].toLowerCase() === 'a' || str[char].toLowerCase() === 'e' || str[char].toLowerCase() === 'i' || str[char].toLowerCase() === 'o' || str[char].toLowerCase() === 'u'){
+//             counter ++
+//         }
+//     }
+//     return counter
+// }
+
+
+// ************************************** Solution 2 ********************************************
 function vowels(str) {
     let counter = 0
-    for (let char in str){
-        if (str[char].toLowerCase() === 'a' || str[char].toLowerCase() === 'e' || str[char].toLowerCase() === 'i' || str[char].toLowerCase() === 'o' || str[char].toLowerCase() === 'u'){
+    const vowel = ['a', 'e', 'i', 'o', 'u']
+
+    for (let char of str.toLowerCase()){
+        if (vowel.includes(char)){
             counter ++
         }
     }
+
     return counter
 }
-
 module.exports = vowels;

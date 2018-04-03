da2d0b1161a560edca0379fedf1bfc1cde469364
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-03 09:15:32 -0500

    vowels iterative solution 1

---
+++ vowels/index.js
@@ -0,0 +1,20 @@
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+// ************************************** Solution 1 ********************************************
+function vowels(str) {
+    let counter = 0
+    for (let char in str){
+        if (str[char].toLowerCase() === 'a' || str[char].toLowerCase() === 'e' || str[char].toLowerCase() === 'i' || str[char].toLowerCase() === 'o' || str[char].toLowerCase() === 'u'){
+            counter ++
+        }
+    }
+    return counter
+}
+
+module.exports = vowels;

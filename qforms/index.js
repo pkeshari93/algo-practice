384a63b7ae61149003f003765fd85f715af91bfc
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-24 09:17:05 -0500

    queue implemented with 2 stacks

--- qforms/index.js
+++ qforms/index.js
@@ -13,6 +13,46 @@
 
 const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(record) {
+    this.stack1.push(record);
+  }
+
+  remove() {
+    while (this.stack1.peek()) {
+      // const record = this.stack1.pop();
+      this.stack2.push(this.stack1.pop());
+    }
+
+    const record = this.stack2.pop();
+
+    while (this.stack2.peek()) {
+      // const record = this.stack1.pop();
+      this.stack1.push(this.stack2.pop());
+    }
+    return record;
+  }
+
+  peek() {
+    while (this.stack1.peek()) {
+      // const record = this.stack1.pop();
+      this.stack2.push(this.stack1.pop());
+    }
+
+    const record = this.stack2.peek();
+
+    while (this.stack2.peek()) {
+      // const record = this.stack1.pop();
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;

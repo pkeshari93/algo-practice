161f9487b5e69e7049e197ce752118df675b996a
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-24 08:29:38 -0500

    implementation complete

--- stack/index.js
+++ stack/index.js
@@ -9,6 +9,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(record) {
+    this.data.unshift(record);
+  }
+
+  pop(record) {
+    return this.data.shift(record);
+  }
+
+  peek() {
+    return this.data[0];
+  }
+}
 
 module.exports = Stack;
